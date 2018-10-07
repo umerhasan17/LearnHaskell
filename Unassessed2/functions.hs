8007ed4ec97762ffcce5cac3a97fab792989c23a
umerhasan17 <umerhasan17@gmail.com>
2018-10-07 01:12:03 +0100

    Adding recursive factorial function and fibonacci however the fibonacci could be improved

--- Unassessed2/functions.hs
+++ Unassessed2/functions.hs
@@ -1,7 +1,25 @@
 type Vertex = (Float, Float)
 
+-- Question 3
 distance :: Vertex -> Vertex -> Float
 -- takes vertex tuples and returns the distance between them
 -- uses Pythagoras
 distance (x1 , y1) (x2 , y2)
   = sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+
+-- Question 6
+fact :: Int -> Int
+-- Pre: x >= 0
+-- maximum factorial is 20! before overflow occurs
+fact x
+  | x == 0    = 1
+  | otherwise = x * fact (x-1)
+
+-- Question 15
+fib :: Int -> Int
+-- Think about improving this function 
+-- There are a lot of redundant calls made
+fib x
+  | x == 0    = 0
+  | x == 1    = 1
+  | otherwise = fib (x-1) + fib (x-2)
