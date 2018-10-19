ca3fec1f792fa3fdaf70845567dff2cfb07f21ae
umerhasan17 <umerhasan17@gmail.com>
2018-10-19 16:30:29 +0100

    Another way of coding the algorithm and storing values using fib table

--- Advanced Programming/session2.hs
+++ Advanced Programming/session2.hs
@@ -13,4 +13,19 @@ fibs
 fibs'
   = 0 : 1 : zipWith (+) fibs' (tail fibs')
 
+-- this feels like a circular dependency
+-- however base cases in fib' solve this
+-- so the index in fibtable at position 0 and 1 exist
 
+fibTable :: [Int]
+fibTable
+  = map fib' [0..]
+
+fib' 0 
+  = 0
+fib' 1
+  = 1
+-- don't call fib prime here because this causes exponential computation
+-- instead index the fib table
+fib' n
+  = fibTable !! (n - 1) + fibTable !! (n - 2)
