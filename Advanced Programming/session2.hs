327797cdd5d5500f88f8fb33e69233ff7f1baa59
umerhasan17 <umerhasan17@gmail.com>
2018-10-19 17:07:51 +0100

    hanoi4 perfect implementation

--- Advanced Programming/session2.hs
+++ Advanced Programming/session2.hs
@@ -45,3 +45,30 @@ hanoi3 n f t v
     [(f, t)] ++
     hanoi3 (n - 1) v t f 
   -- to is the via 
+
+-- it is believed that the following is optimal for n = 4
+-- hasn't been proven
+-- set aside k sorted elements to spare peg
+-- do hanoi 3 with the rest of the elements
+-- use the 4 strategy to get the k sorted elements onto the peg 
+
+hanoi4 :: Int -> Int -> Int -> Int -> Int -> [(Int, Int)]
+-- from to via 
+hanoi4 0 f t v v'
+  = []
+hanoi4 n f t v v'
+  = hanoi4 k f v' t v ++
+    hanoi3 (n - k) f t v ++
+    hanoi4 k v' t f v
+  where
+    k = snd(ks !! n)
+
+ks :: [(Int, Int)]
+ks
+  = (0, 0) : (1, 0) : map makePair [2..]
+  where
+    makePair n = minimum [ (cost n k, k) | k <- [0 .. n -1]]
+    cost n k   = 2 * fst (ks !! k) + 2 ^ (n - k) - 1
+
+-- [length ( hanoi4 n 1 2 3 4) | n <- [1..10]]
+-- [1,3,5,9,13,17,25,33,41,49]
