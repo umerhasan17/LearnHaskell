ce5fd7afc69e0b6e3aead3fb28ef00b4ec57fca9
umerhasan17 <umerhasan17@gmail.com>
2018-11-08 10:30:42 +0000

    Attempt at qus.hs 6

--- Unassessed6/questions.hs
+++ Unassessed6/questions.hs
@@ -1,3 +1,9 @@
+-- how to find list of all functions in GHC.Enum
+-- derive specific functions in classes
+-- 
+
 data Colour = Red | Green | Blue
             deriving (Show, Bounded)
 
+instance Bounded Colour where
+  minBound colour = Red
