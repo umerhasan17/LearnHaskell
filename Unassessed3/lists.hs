2da1a8a70e460fdfc35746a5afd4235b306eb394
umerhasan17 <umerhasan17@gmail.com>
2018-11-09 11:48:56 +0000

    More unassess 3

--- Unassessed3/lists.hs
+++ Unassessed3/lists.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 pos :: Int -> [Int] -> Int
 pos _ []
   = error "Index out of range"
@@ -15,3 +17,78 @@ rev []
   = []
 rev (x : xs)
   = rev xs ++ x
+
+-- List Comprehensions
+
+findAll x t
+  = [y | (x', y) <- t, x == x']
+
+remove :: Eq a => a -> [(a, b)] -> [(a, b)]
+remove x table
+  = filter ( (/=x).fst) table
+  where
+    -- p      = 
+    toDrop = [y | y@(x', _) <- table, x' /= x]
+
+quicksort :: (Eq a) => (Ord a) => [a] -> [a]
+quicksort []
+  = []
+quicksort (x : xs)
+  = lesser ++ (x : greater)
+  where
+    lesser  = quicksort (filter (<=x) xs)
+    greater = quicksort (filter (>x) xs)
+
+splitUp :: String -> [String]
+splitUp "" = []
+splitUp s
+  = w : splitUp ws
+  where
+    (w, ws) = nextWord (removeWhitespace s)
+
+removeWhitespace :: String -> String
+removeWhitespace ""
+  = ""
+removeWhitespace s@(c : cs)
+  | isSpace c = removeWhitespace cs
+  | otherwise = s
+
+isSpace :: Char -> Bool
+isSpace c
+  = (c == ' ')
+
+nextWord :: String -> (String, String)
+--Pre: The first character is non-whitespace
+nextWord ""
+  = ("", "")
+nextWord (c : cs)
+  | isSpace c = ("", cs)
+  | otherwise = (c : w, s)
+  where
+    (w, s) = nextWord cs
+
+allSplits :: [a] -> [([a], [a])]
+allSplits xs
+  = [splitAt x xs | x <- [1..length(xs) - 1]]
+
+prefixes :: [t] -> [[t]]
+prefixes xs
+  = [take n xs | n <- [1..length(xs)]] 
+
+substrings :: String -> [String]
+substrings []
+  = []
+substrings xs
+  = prefixes xs ++ substrings (tail xs)
+
+-- perms :: [a] -> [[a]]
+-- perms xs
+--   = 
+
+-- tails :: [a] -> [[a]]
+-- tails xs
+--  = [xs]
+
+substrings2 :: String -> [String]
+substrings2 s
+  = [i | t <- tails s, i <- tail (inits t)]
