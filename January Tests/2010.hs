52c488ae23ae01626b0170e6c6b56b58b821ffa6
umerhasan17 <umerhasan17@gmail.com>
2018-12-16 18:55:01 +0000

    removePrefix

--- January Tests/2010.hs
+++ January Tests/2010.hs
@@ -14,9 +14,11 @@ isPrefix (x : xs) (y : ys)
   = (x == y) && isPrefix xs ys
 
 removePrefix :: String -> String -> String
-removePrefix
 --Pre: s is a prefix of s'
-  = 
+removePrefix [] ys
+  = ys
+removePrefix (x : xs) (y: ys)
+  = removePrefix xs ys
 
 suffixes :: [a] -> [[a]]
 suffixes
