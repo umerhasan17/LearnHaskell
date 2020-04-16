91a13d917cb0f1e81abd66944a814cee32c8f18e
umerhasan17 <umerhasan17@gmail.com>
2020-04-16 19:03:57 +0100

    Finish qu 1 mock

--- algorithms/mock1.hs
+++ algorithms/mock1.hs
@@ -24,30 +24,57 @@ dist (x: xs) (y: ys)
 
 -- Question 1b
 
+tabulate2d:: (Num i, Enum i, Ix i) => (i, i) -> ((i, i) -> a) -> Array (i, i) a
+tabulate2d (m, n) f = array ((0, 0), (n, m)) [((x, y), (f (x, y))) | x <- range(0, m), y <- range(0, n)]
+
 tabulate:: (Enum i, Ix i) => (i, i) -> (i -> a) -> Array i a
 tabulate (m, n) f = array (m, n) [(i, f i) | i <- range(m, n)]
 
 dist':: String -> String -> Int
 dist' xs ys
     -- = table
-    = table ! (length xs * (length ys+1))
+    = table ! (length xs, length ys)
     where
-        table = tabulate (0, length xs * (length ys+1)) mdist
-        mdist:: Int -> Int
-        mdist index
-            | index == 0  = 0
-            | index < length xs          = index
-            | index `mod` (length xs+1) == 0 = index - (xIndex * length xs)
-            | charsEqual                 = op3
-            | otherwise                  = 1 + minimum options
+        table = tabulate2d (length xs, length ys) mdist
+        mdist:: (Int, Int) -> Int
+        mdist (0, 0)  = 0
+        mdist (0, x) = x
+        mdist (x, 0) = x
+        mdist (x, y) 
+            | sameChar  = table ! (x-1, y-1)
+            | otherwise = 1 + minimum options
             where
-                (xIndex, yIndex) = divMod index (length xs)
-                charsEqual = xIndex > 0 && yIndex > 0 && xs !! (xIndex-1) == ys !! (yIndex-1)
-                options = [op1, op2, op3]
-                -- left most column case already accounted for
-                op1 = table ! (index - 1) 
-                op2 = table ! (index - length xs)
-                op3 = table ! (index - (length xs + 1) - 1)
+                sameChar = (xs !! (x-1)) == (ys !! (y-1))
+                options = [table ! (x-1, y-1), table ! (x, y-1), table ! (x-1, y)]
+
+--   dist [] []
+--     = 0
+-- dist [] xs
+--     = length xs
+-- dist xs []
+--     = length xs
+-- dist (x: xs) (y: ys)
+--     | x == y    = dist xs ys
+--     | otherwise = 1 + minimum options
+--     where
+--         options = [op1, op2, op3]
+--         op1 = dist xs (y: ys)
+--         op2 = dist (x: xs) ys
+--         op3 = dist xs ys
+
+--             | index == 0  = 0
+--             | index < length xs          = index
+--             | index `mod` (length xs+1) == 0 = index - (xIndex * length xs)
+--             | charsEqual                 = op3
+--             | otherwise                  = 1 + minimum options
+--             where
+--                 (xIndex, yIndex) = divMod index (length xs)
+--                 charsEqual = xIndex > 0 && yIndex > 0 && xs !! (xIndex-1) == ys !! (yIndex-1)
+--                 options = [op1, op2, op3]
+--                 -- left most column case already accounted for
+--                 op1 = table ! (index - 1) 
+--                 op2 = table ! (index - length xs)
+--                 op3 = table ! (index - (length xs + 1) - 1)
 
         -- mdist [] []
         --     = 0
