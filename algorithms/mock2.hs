5901344613034c348467f5733f7719956c43cdd8
umerhasan17 <umerhasan17@gmail.com>
2020-04-20 23:18:32 +0100

    Add sqrt 2 solution

--- algorithms/mock2.hs
+++ algorithms/mock2.hs
@@ -1,3 +1,5 @@
+import System.Random
+
 fib :: Int -> Integer
 fib 0 = 0
 fib 1 = 1
@@ -23,7 +25,7 @@ fib'' n
             | otherwise = (k ** 2) * (phi 1)
             where
                 k = phi (n `div` 2)
-    
+
 -- mkStdGen :: Int → StdGen
 -- creates a seed value given an integer
 
@@ -32,3 +34,28 @@ fib'' n
 
 -- randomR :: Random a ⇒ (a, a) → StdGen → (a, StdGen)
 -- generates a number between low high range given and returns number with new stdgen
+
+-- sqrt5:: Double
+-- sqrt5 = sqrt5' (mkStdGen 42) 0 0
+--     where
+--         -- stores below, total
+--         sqrt5' :: StdGen -> Int -> Int -> Double
+--         sqrt5' s m n
+--             | n == 100000   = 3 * (fromIntegral m / fromIntegral  n)
+--             | (r ** 2) <= 5.0 = sqrt5' s' (m+1) (n+1) 
+--             | otherwise     = sqrt5' s' m (n+1)
+--             where
+--                 (r, s') = randomR (0 3) s
+
+root2 :: Double
+root2 = loop (mkStdGen 42) 0 0
+    where
+        loop seed m n
+            | n == 100000 = 2 * m / fromIntegral n
+            | otherwise   = loop seed' m' n'
+            where 
+                n' = n+1
+                m' = if inside x then m + 1 else m
+                (x,seed') = randomR (0, 2) seed
+                inside :: Double -> Bool
+                inside x = x * x <= 2
